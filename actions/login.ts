e308beabda4505ec5bcb56351b90dbc4acdb81af
Eddie <eddie.zeng95@gmail.com>
2024-06-29 14:57:48 +1000

    Redirect from home page to settings

--- actions/login.ts
+++ actions/login.ts
@@ -20,7 +20,7 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     }
 
     const { email, password, code } = validatedFields.data;
-
+    
     const existingUser = await getUserByEmail(email);
 
     if (!existingUser || !existingUser.email || !existingUser.password) {
